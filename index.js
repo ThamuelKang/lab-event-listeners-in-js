de74929cf45d6d8921232e160c9dff72f4c6aacd
Samuel Kang <samuel.june.kang@gmail.com>
2025-06-12 14:27:55 -0700

    First commit

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 // Handle Button Clicks
+const backgroundButton = document.getElementById("changeColorButton")
+const resetButton = document.getElementById("resetColorButton")
+const textDisplay = document.getElementById("textInputDisplay")
+const textInput = document.getElementById("textInput")
+const keyDisplay = document.getElementById("keyPressDisplay")
 
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  document.body.style.backgroundColor = '#ffd86e';
 }
+backgroundButton.addEventListener("click", changeBackgroundColor);
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
-  // Implement the function to reset background color
+  document.body.style.backgroundColor = 'white'
 }
+resetButton.addEventListener("click", resetBackgroundColor)
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  keyDisplay.textContent = event.key
+
 }
+document.addEventListener("keydown", displayKeyPress)
 
 // Process Text Input
 
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  textDisplay.textContent = textInput.value
 }
+textInput.addEventListener("input", displayUserInput)
 
 // Attach Event Listeners
 
